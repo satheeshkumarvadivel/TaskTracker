9268b0215c746de9184d158631b847df4a017d31
Dinesh Ilango (dinilang) <dinilang@cisco.com>
2022-09-16 11:44:03 +0530

    TA009: Integrating View categories with api

---
+++ src/main/resources/static/js/GetTaskGroups.js
@@ -0,0 +1,33 @@
+class GetTaskGroups extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            groups: []
+        };
+    }
+
+    componentDidMount() {
+        let axiosConfig = {
+                    headers: {
+                      'Content-Type': 'application/json;charset=UTF-8',
+                    }
+                };
+
+        axios.get('/api/v1/users/1/taskgroups')
+            .then((response) => {
+                var arr = []
+                response.data.forEach(obj => {
+                    arr.push({value: obj.id, label: obj.groupName});
+                });
+                this.setState({ groups: arr });
+            }
+        );
+    }
+
+    render() {
+        return (<select id="inputState" className="form-control" defaultValue="2">{this.state.groups.map(item => <option key={item.value}>{item.label}</option>)}</select>);
+    }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<GetTaskGroups />);
